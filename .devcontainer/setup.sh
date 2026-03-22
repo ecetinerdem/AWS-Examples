f2f29f2b7db87603a8e3098242b44cafcf73a7f7
Cetin Erdem <e.cetinerdem@gmail.com>
2026-03-22 09:52:33 +0000

    devcontainer setup and aws-cli install

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,15 @@
+#!bin/bash
+
+cd /tmp
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+
+cd $GITHUB_WORKSPACE
+
+aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
+aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
+aws configure set default.region $AWS_DEFAULT_REGION
+
+echo "AWS CLI installed and configured!"
+aws --version
