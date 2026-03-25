a8d63bbbff532b5ce08f72dfd02ffa2b5646c33d
Cetin Erdem <e.cetinerdem@gmail.com>
2026-03-25 14:37:55 +0000

    create bucket

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,8 +1,8 @@
-#!bin/bash
+#!/usr/bin/env bash
 
 cd /tmp
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
+unzip -o awscliv2.zip
 sudo ./aws/install
 
 cd $GITHUB_WORKSPACE
