36bfb5ef25aef29940a323cfe78ab505d574db73
Cetin Erdem <e.cetinerdem@gmail.com>
2026-04-10 16:18:41 +0000

    add object via terraform to s3

---
+++ s3/etags/main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.40.0"
+    }
+  }
+}
+
+provider "aws" {
+
+}
+
+resource "aws_s3_bucket" "default" {
+}
+
+
+resource "aws_s3_object" "object" {
+  bucket = aws_s3_bucket default.id
+  key    = "myfile.txt"
+  source = "myfile.txt"
+
+  etag = filemd5("myfile.txt)
+}
+
+
